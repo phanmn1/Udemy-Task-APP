60cb36eb687f795198f65a32de109d2c7a36abb4
Minh Phan <mpflamingmarshmallow@gmail.com>
2019-04-11 10:40:52 -0700

    Changed jwt signature for env variables

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
     try {
         //Tutorial uses req.header('Authorization')
         const token = req.headers.authorization.replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const user = await User.findOne({_id: decoded._id, 'tokens.token' : token})
 
